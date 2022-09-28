1a8140101e939477b52c4d5685e451462de0cbfd
Burak Aksoy <burakaksoy20@gmail.com>
2022-09-28 17:35:40 -0400

    debug

--- src/level_sensor_service/level_sensor_service.py
+++ src/level_sensor_service/level_sensor_service.py
@@ -81,18 +81,25 @@ class LevelSensor_impl():
     # Returns ...
     def isLevelLow(self):
         # Read image from ros topic
-        depth_array = self.ros_image_subscriber.get_latest_image()
-        # print("depth_array is of type:", type(depth_array))
-        # print(str(depth_array))
-        print("")
-        print("image h,w: " + str(depth_array.shape))
+        num_readings = 10
+        levels = np.zeros(1,num_readings)
+        for i in range(num_readings):
+            depth_array = self.ros_image_subscriber.get_latest_image()
+            # print("depth_array is of type:", type(depth_array))
+            # print(str(depth_array))
+            print("")
+            print("image h,w: " + str(depth_array.shape))
 
-        depth_array_roi = depth_array[self.roi_y:self.roi_y+self.roi_h,self.roi_x:self.roi_x+self.roi_w]
-        # print("depth_array_roi is of type:", type(depth_array_roi))
-        # print(str(depth_array_roi))
-        print("ROI image h,w: " + str(depth_array_roi.shape))
+            depth_array_roi = depth_array[self.roi_y:self.roi_y+self.roi_h,self.roi_x:self.roi_x+self.roi_w]
+            # print("depth_array_roi is of type:", type(depth_array_roi))
+            # print(str(depth_array_roi))
+            print("ROI image h,w: " + str(depth_array_roi.shape))
 
-        avr_level = np.mean(depth_array_roi)
+            avr_level = np.mean(depth_array_roi)
+            levels[i] = avr_level
+
+        print("levels: " + str(levels))
+        avr_level = np.mean(levels)
         print("avr_level: "+str(avr_level))
         print("low_level: "+str(self.low_level))
 
@@ -104,18 +111,25 @@ class LevelSensor_impl():
     # Returns ...
     def isLevelHigh(self):
         # Read image from ros topic
-        depth_array = self.ros_image_subscriber.get_latest_image()
-        # print("depth_array is of type:", type(depth_array))
-        # print(str(depth_array))
-        print("")
-        print("image h,w: " + str(depth_array.shape))
+        num_readings = 10
+        levels = np.zeros(1,num_readings)
+        for i in range(num_readings):
+            depth_array = self.ros_image_subscriber.get_latest_image()
+            # print("depth_array is of type:", type(depth_array))
+            # print(str(depth_array))
+            print("")
+            print("image h,w: " + str(depth_array.shape))
 
-        depth_array_roi = depth_array[self.roi_y:self.roi_y+self.roi_h,self.roi_x:self.roi_x+self.roi_w]
-        # print("depth_array_roi is of type:", type(depth_array_roi))
-        # print(str(depth_array_roi))
-        print("ROI image h,w: " + str(depth_array_roi.shape))
+            depth_array_roi = depth_array[self.roi_y:self.roi_y+self.roi_h,self.roi_x:self.roi_x+self.roi_w]
+            # print("depth_array_roi is of type:", type(depth_array_roi))
+            # print(str(depth_array_roi))
+            print("ROI image h,w: " + str(depth_array_roi.shape))
 
-        avr_level = np.mean(depth_array_roi)
+            avr_level = np.mean(depth_array_roi)
+            levels[i] = avr_level
+
+        print("levels: " + str(levels))
+        avr_level = np.mean(levels)
         print("avr_level: "+str(avr_level))
         print("high_level: "+str(self.high_level))
 
