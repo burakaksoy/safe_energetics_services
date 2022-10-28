525e5b197e1598471b2219e6d66398296e0f7a32
Burak Aksoy <burakaksoy20@gmail.com>
2022-10-28 11:50:26 -0400

    level sens client example in while loop

--- src/level_sensor_service/level_sensor_client_example.py
+++ src/level_sensor_service/level_sensor_client_example.py
@@ -12,17 +12,18 @@ def main():
     #Start up Robot Raconteur and connect, standard by this point    
     c = RRN.ConnectService(url)
 
-    level_low = c.isLevelLow()
-    if level_low:
-        print("Hopper level is LOW")
-    else:
-        print("Hopper level is GOOD")
+    while True:
+        level_low = c.isLevelLow()
+        if level_low:
+            print("Hopper level is LOW")
+        else:
+            print("Hopper level is GOOD")
 
-    level_high = c.isLevelHigh()
-    if level_high:
-        print("Hopper level is HIGH")
-    else:
-        print("Hopper level is not HIGH, more material can be added")
+        level_high = c.isLevelHigh()
+        if level_high:
+            print("Hopper level is HIGH")
+        else:
+            print("Hopper level is not HIGH, more material can be added")
 
 
 if __name__ == '__main__':
