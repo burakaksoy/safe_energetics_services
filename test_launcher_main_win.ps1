949e761ef56a3f71f6597a78ef8ba238282e1c9e
Burak Aksoy <burakaksoy20@gmail.com>
2022-11-30 10:20:36 -0500

    debug

--- test_launcher_main_win.ps1
+++ test_launcher_main_win.ps1
@@ -7,6 +7,7 @@ wt -w 0 nt Powershell -NoExit -c {
 
 # IO module service
 wt -w 0 nt Powershell -NoExit -c {
+    Start-Sleep -Seconds 1.5
     $Host.UI.RawUI.WindowTitle = "IO Service"
     cd ~\safe_energetics_services\src\io_manager_service
     python.exe .\io_manager_service.py
@@ -14,6 +15,7 @@ wt -w 0 nt Powershell -NoExit -c {
 
 # Robot request client example
 wt -w 0 nt Powershell -NoExit -c {
+    Start-Sleep -Seconds 3.0
     $Host.UI.RawUI.WindowTitle = "Robot Request Example Client"
     cd ~\safe_energetics_services\src\robot_request_service
     python.exe .\robot_request_client_interactive.py
@@ -21,6 +23,7 @@ wt -w 0 nt Powershell -NoExit -c {
 
 # IO module client example
 wt -w 0 nt Powershell -NoExit -c {
+    Start-Sleep -Seconds 3.0
     $Host.UI.RawUI.WindowTitle = "IO Example Client"
     cd ~\safe_energetics_services\src\io_manager_service
     python.exe .\io_manager_client_interactive.py
