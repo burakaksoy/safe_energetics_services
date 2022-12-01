13ed158250f76ae2cc6df834df0167dbd5a7b06f
Burak Aksoy <burakaksoy20@gmail.com>
2022-12-01 16:35:50 -0500

    debug

--- all_devices_git_pull.bash
+++ all_devices_git_pull.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 # HOSTS=("192.168.1.139" "192.168.1.141" "192.168.1.216") # wifi
-HOSTS=("192.168.1.140" "192.168.1.142" "192.168.1.216") # ethernet
+HOSTS=("192.168.1.140" "192.168.1.142" "192.168.1.85") # ethernet
 # HOSTS=("192.168.55.10" "192.168.55.11" "192.168.1.216") # abb ethernet
-USERNAMES=("jetson_silver" "jetson_blue" "TODO")
-PASSWORDS=("1234" "1234" "1234" )
+USERNAMES=("jetson_silver" "jetson_blue" "wgc")
+PASSWORDS=("1234" "1234" "WGCat90%" )
 
 SCRIPTS=("cd ~/catkin_ws_kinect/src/safe_energetics_services; 
           git reset --hard; git pull;"
@@ -11,7 +11,7 @@ SCRIPTS=("cd ~/catkin_ws_kinect/src/safe_energetics_services;
           "cd ~/catkin_ws_kinect/src/safe_energetics_services; 
           git reset --hard; git pull;"
           
-          "cd ~/catkin_ws_kinect/src/safe_energetics_services; 
+          "cd ~/safe_energetics_services; 
           git reset --hard; git pull;")
 for i in ${!HOSTS[*]} ; do
     echo "------------"
